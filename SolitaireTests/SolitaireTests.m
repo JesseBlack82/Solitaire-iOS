6aa56d15257799992a9c3f8075702b6fc4d7a162
Jesse Black <jesse.black@stablekernel.com>
2015-07-05 05:31:18 -0400

    Tests for which cards can be picked up

--- SolitaireTests/SolitaireTests.m
+++ SolitaireTests/SolitaireTests.m
@@ -1,4 +1,3 @@
-
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 #import "STKBoard.h"
@@ -16,7 +15,8 @@
 
 @implementation SolitaireTests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -25,7 +25,8 @@
     [[self engine] dealCards:[STKCard deck]];
 }
 
-- (void)tearDown {
+- (void)tearDown
+{
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [self setBoard:nil];
     [self setEngine:nil];
@@ -33,15 +34,18 @@
     [super tearDown];
 }
 
-- (void)testStockHas24CardsAfterSetup {
+- (void)testStockHas24CardsAfterSetup
+{
     XCTAssertEqual(24, [[[self engine] stock] count]);
 }
 
-- (void)testWasteHas0CardsAfterSetup {
+- (void)testWasteHas0CardsAfterSetup
+{
     XCTAssertEqual(0, [[[self engine] waste] count]);
 }
 
-- (void)testFoundationsHave0CardsAfterSetup {
+- (void)testFoundationsHave0CardsAfterSetup
+{
     for (NSArray *foundation in [[self board] foundations]) {
         XCTAssertEqual(0, [foundation count]);
     }
@@ -55,17 +59,20 @@
     }
 }
 
-- (void)testTableausHave1CardAfterSetup {
+- (void)testTableausHave1CardAfterSetup
+{
     for (NSArray *tableau in [[self board] tableaus]) {
         XCTAssertEqual(1, [tableau count]);
     }
 }
 
-- (void)testCanDrawStockToWasteWhenStockIsNonEmpty {
+- (void)testCanDrawStockToWasteWhenStockIsNonEmpty
+{
     XCTAssertTrue([[self engine] canDrawStockToWaste]);
 }
 
-- (void)testCanNotDrawStockToWasteWhenStockIsEmpty {
+- (void)testCanNotDrawStockToWasteWhenStockIsEmpty
+{
     while ([[[self engine] stock] count] > 0) {
         [STKBoard moveTopCard:[[self board] stock] toPile:[[self board] waste]];
     }
@@ -73,7 +80,8 @@
     XCTAssertFalse([[self engine] canDrawStockToWaste]);
 }
 
-- (void)testCanRedealWasteToStockWhenStockIsEmptyAndWasteIsNonEmpty {
+- (void)testCanRedealWasteToStockWhenStockIsEmptyAndWasteIsNonEmpty
+{
     while ([[[self engine] stock] count] > 0) {
         [STKBoard moveTopCard:[[self board] stock] toPile:[[self board] waste]];
     }
@@ -81,18 +89,96 @@
     XCTAssertTrue([[self engine] canResetWasteToStock]);
 }
 
-- (void)testCanNotRedealWasteToStockWhenStockIsNonEmpty {
+- (void)testCanNotRedealWasteToStockWhenStockIsNonEmpty
+{
     // stock is already none empty, make sure to populate waste to make the next test more valuable
     [STKBoard moveTopCard:[[self board] stock] toPile:[[self board] waste]];
 
     XCTAssertFalse([[self engine] canResetWasteToStock]);
 }
 
-- (void)testCanNotRedealWasteToStockWhenWasteIsEmpty {
+- (void)testCanNotRedealWasteToStockWhenWasteIsEmpty
+{
     [[[self board] stock] removeAllObjects];
     [[[self board] waste] removeAllObjects];
 
     XCTAssertFalse([[self engine] canResetWasteToStock]);
+}
+
+- (void)testCanGrabTopWasteCard
+{
+    [STKBoard moveTopCard:[[self board] stock] toPile:[[self board] waste]];
+
+    STKCard *topWasteCard = [[[self engine] waste] lastObject];
+    XCTAssertTrue([[self engine] canGrab:topWasteCard]);
+}
+
+- (void)testCanNotGrabCoveredWasteCard
+{
+    while ([[[self engine] stock] count]) {
+        [STKBoard moveTopCard:[[self board] stock] toPile:[[self board] waste]];
+    }
+
+    for (STKCard *card in [[self engine] waste]) {
+        if (card != [[[self engine] waste] lastObject]) {
+            XCTAssertFalse([[self engine] canGrab:card]);
+        }
+    }
+}
+
+- (void)testCanGrabTopFoundationCards
+{
+    for (NSMutableArray *foundation in [[self board] foundations]) {
+        [STKBoard moveTopCard:[[self board] stock] toPile:foundation];
+
+        XCTAssertTrue([[self engine] canGrab:[foundation lastObject]]);
+    }
+}
+
+- (void)testCanNotGrabCoveredFoundationCards
+{
+    while ([[[self engine] stock] count] > 0) {
+        for (NSMutableArray *foundation in [[self board] foundations]) {
+            [STKBoard moveTopCard:[[self board] stock] toPile:foundation];
+        }
+    }
+
+    for (NSArray *foundation in [[self engine] foundations]) {
+        for (int i = 1; i < [foundation count]; ++i) {
+            XCTAssertFalse([[self engine] canGrab:[foundation objectAtIndex:0]]);
+        }
+    }
+}
+
+- (void)testCanGrabTopTableauCards
+{
+    while ([[[self engine] stock] count] > 0) {
+        for (NSMutableArray *tableau in [[self board] tableaus]) {
+            [STKBoard moveTopCard:[[self board] stock] toPile:tableau];
+        }
+    }
+
+    for (NSArray *tableau in [[self engine] tableaus]) {
+        XCTAssertTrue([[self engine] canGrab:[tableau lastObject]]);
+    }
+}
+
+- (void)testCanGrabCoveredCardsFromTableau // from covered card all the way to top
+{
+    while ([[[self engine] stock] count] > 0) {
+        for (NSMutableArray *tableau in [[self board] tableaus]) {
+            [STKBoard moveTopCard:[[self board] stock] toPile:tableau];
+        }
+    }
+
+    for (NSArray *tableau in [[self engine] tableaus]) {
+        for (STKCard *card in tableau) {
+            if (card == [tableau lastObject]) {
+                return;
+            }
+            XCTAssertTrue([[self engine] canGrab:[tableau lastObject]]);
+        }
+    }
 }
 
 @end
